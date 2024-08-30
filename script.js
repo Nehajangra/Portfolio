2a87df3d58275fb4f65425e6bfa4dbf0824ddd7f
Neha Jangra <developernehajangra48@gmail.com>
2024-08-30 23:27:36 +0530

    experience section

--- script.js
+++ script.js
@@ -7,3 +7,6 @@ function hideSidebar(){
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display="none";
 }
+// document.querySelector('.burger').addEventListener('click', () => {
+//     document.querySelector('.nav-links').classList.toggle('active');
+// });
